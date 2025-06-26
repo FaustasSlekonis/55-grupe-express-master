74145b5b8b7eba24c64da4e98ab296227d6fc0bb
Faustas <faustas.slekonis@gmail.com>
2025-06-26 13:27:26 +0300

    express

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import { PageTeam } from './pages/PageTeam.js';
 import { registerAPI } from './api/registerAPI.js';
 import { PageLogin } from './pages/PageLogin.js';
 import { loginAPI } from './api/loginAPI.js';
+import { PageDashboard } from './pages/PageDashboard.js';
+import { loginTokens } from './data/users.js';
+import { cookieParser } from './middleware/cookieParser.js';
+import { userData } from './middleware/userData.js';
 
 const app = express();
 const port = 3000;
@@ -17,6 +21,10 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.json());
 
+app.use(cookieParser);
+app.use(userData);
+
+// public routes
 app.get('/', (req, res) => res.send(new PageHome(req).render()));
 app.get('/about', (req, res) => res.send(new PageAbout(req).render()));
 app.get('/services', (req, res) => res.send(new PageServices(req).render()));
@@ -26,9 +34,14 @@ app.get('/team', (req, res) => res.send(new PageTeam(req).render()));
 app.get('/register', (req, res) => res.send(new PageRegister(req).render()));
 app.get('/login', (req, res) => res.send(new PageLogin(req).render()));
 
+// private routes
+app.get('/dashboard', (req, res) => res.send(new PageDashboard(req).render()));
+
+// api routes
 app.post('/api/register', registerAPI);
 app.post('/api/login', loginAPI);
 
+// error
 app.get('*error', (req, res) => res.send(new Page404(req).render()));
 
 app.listen(port, () => {
